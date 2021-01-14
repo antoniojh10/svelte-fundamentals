9811461351cd24362ed88dca35389b3bcb63a37f
antoniojh10 <antoniohernandez95@gmail.com>
2021-01-14 01:24:30 -0400

    if-else and darkmode in svelte

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -3,15 +3,28 @@
   let someText = `Frontend Developer at Platzi`;
   let count = 0;
 
+  let styles = { darkMode: false };
+
   function handleClick() {
     count = count + 1;
+  }
+
+  function toggle() {
+    styles.darkMode = !styles.darkMode;
+    window.document.body.classList.toggle("dark-mode");
   }
 </script>
 
 <div class="About">
-  <p>{someText}</p>
+  {#if !styles.darkMode}
+    <p>{someText}</p>
+  {:else}
+    <p>Hello Dark Mode</p>
+  {/if}
+
   <h1>{count === 0 ? "Haz click abajo!" : count}</h1>
   <button on:click={handleClick}>Click</button>
+  <button on:click={toggle}>Dark Mode: {styles.darkMode ? "on" : "off"}</button>
 </div>
 
 <style>
