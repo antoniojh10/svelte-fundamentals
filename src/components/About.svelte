3018aac3b09d17cb0db54d9069c3fd2ce747b104
antoniojh10 <antoniohernandez95@gmail.com>
2021-01-14 01:00:35 -0400

    reactivity in svelte

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,10 +1,17 @@
 <script>
   // Logica
   let someText = `Frontend Developer at Platzi`;
+  let count = 0;
+
+  function handleClick() {
+    count = count + 1;
+  }
 </script>
 
 <div class="About">
   <p>{someText}</p>
+  <h1>{count === 0 ? "Haz click abajo!" : count}</h1>
+  <button on:click={handleClick}>Click</button>
 </div>
 
 <style>
